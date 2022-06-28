5699e53c432cc22e01133fadab1028eb8894aaeb
PietyE <kvasovdim17@gmail.com>
2022-06-28 12:48:09 +0300

    added linters:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="wrapper">
-
-    </div>
-  );
-}
+const App = () => <div className="wrapper"></div>;
 
 export default App;
